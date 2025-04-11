b92d8d0c0487abdf1f96d393319a7b46b7798036
Carlos Miguel Tadifa <cdtadifa21@gmail.com>
2025-04-11 20:54:58 +0800

    first test for selenium

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Setup ChromeDriver using WebDriverManager
+        WebDriverManager.chromedriver().setup();
+
+        // Launch Chrome browser
+        WebDriver driver = new ChromeDriver();
+
+        // Open a website
+        driver.get("https://parabank.parasoft.com");
+
+        // Print the page title to check if it loaded successfully
+        System.out.println("Page title: " + driver.getTitle());
+
+        // Close browser
+        driver.quit();
     }
 }
