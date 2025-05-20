d3415ebc6b7536babd6d4ac6ce421b2be7c5bc0b
Carlos Miguel Tadifa <cdtadifa21@gmail.com>
2025-05-20 20:12:02 +0800

    Completed TCM10 paybilltest to enhance

--- src/test/java/testcase/services/payBillTest.java
+++ src/test/java/testcase/services/payBillTest.java
@@ -169,27 +169,8 @@ public class payBillTest extends paybillbasepage{
         accService.clickSendPayment();
     }
 
-    @Test(description = "Verify bill payment with negative amount")
-    public void TCM9() {
-        accService = new accountServices(driver);
-        api = new apiPOM();
-        accountServices.pInfoData data = accService.new pInfoData();
-        data.payeeName = fullName;
-        data.street = street;
-        data.city = city;
-        data.state = state;
-        data.zipCode = zipcode;
-        data.phone = phone;
-        data.accno = "123";
-        data.vaccno = "123";
-        data.amount = "-123";
-
-        accService.payeeInfo(data, 0);
-        accService.clickSendPayment();
-    }
-
     @Test(description = "Verify paying using 2nd account #")
-    public void TCM10() {
+    public void TCM9() {
         accService = new accountServices(driver);
         api = new apiPOM();
         api.postCreateAccount(userName, passWord, "checking");
@@ -208,14 +189,36 @@ public class payBillTest extends paybillbasepage{
         accService.payeeInfo(data, 1);
         accService.clickSendPayment();
     }
+
+    @Test(description = "Verify paying using 2nd account #")
+    public void TCM10() {
+        accService = new accountServices(driver);
+        api = new apiPOM();
+        accountServices.pInfoData data = accService.new pInfoData();
+        data.payeeName = fullName;
+        data.street = street;
+        data.city = city;
+        data.state = state;
+        data.zipCode = zipcode;
+        data.phone = phone;
+        data.accno = "123";
+        data.vaccno = "123";
+        data.amount = "123";
+
+        for(int x=0; x<2; x++) {
+            String balance = api.getAccountBalance(userName, passWord);
+            accService.selectAccountServices("billPay"); // to enhance
+            accService.payeeInfo(data, 0);
+            accService.clickSendPayment();
+            accService.checkBalance(userName, passWord, balance, data);
+        }
+    }
 }
 
 
 
 
 /*
-    Verify UI formatting for currency in amount
-    Verify success message after payment
     Verify multiple bill payments can be made one after another
     Verify account balance is updated after bill payment
  */
