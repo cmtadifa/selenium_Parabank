ceff79a52a00bab29e3e2d79bb52fdc737f654fd
Carlos Miguel Tadifa <cdtadifa21@gmail.com>
2025-05-22 22:06:34 +0800

    login Test TCM's

---
+++ src/test/java/testcase/services/loginTest.java
@@ -0,0 +1,24 @@
+package testcase.services;
+import org.testng.annotations.Test;
+import testcase.base.paybillbasepage;
+import testcase.pom.accountServices;
+import testcase.pom.apiPOM;
+
+
+public class loginTest {
+
+
+}
+
+/*
+Verify successful login with valid credentials
+Verify login with invalid username
+Verify login with invalid password
+Verify login with both username and password incorrect
+Verify login with blank username and password
+Verify login with only username entered
+Verify login with only password entered
+Verify login with special characters in username
+Verify login functionality after logout
+
+ */
