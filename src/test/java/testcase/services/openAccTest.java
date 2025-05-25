3ce35267da0adb00ce68728726e12d20ea2ffd41
Carlos Miguel Tadifa <cdtadifa21@gmail.com>
2025-05-25 10:40:50 +0800

    creating main test file for open account

---
+++ src/test/java/testcase/services/openAccTest.java
@@ -0,0 +1,28 @@
+package testcase.services;
+import org.testng.annotations.Test;
+import testcase.base.basepage;
+import testcase.pom.accountServices;
+import testcase.pom.homepage;
+
+
+public class openAccTest extends basepage{
+    accountServices accService;
+    homepage homePage;
+
+    /*
+    Verify opening a Checking account with valid amount
+    Verify opening a Checking account with exact required amount
+    Verify opening a Checking account with blank amount field
+    Verify opening a Checking account with alphanumeric amount
+    Verify opening a Checking account with less than the required amount
+    Verify opening a Savings account with valid amount
+    Verify opening a Savings account with exact required amount
+    Verify opening a Savings account with blank amount field
+    Verify opening a Savings account with alphanumeric amount
+    Verify opening a Savings account with less than the required amount
+    Verify opening a Savings and Checking account using the first account
+    Verify opening a new account using the other opened account
+    Verify account dropdown with 3 opened account to be the same  with the accounts overview
+     */
+
+}
